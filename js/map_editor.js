7c57f537f3230f77a8f49057a5af84240aebc664
Andrei <andrei.harpa.ah@gmail.com>
2018-01-29 16:07:25 +0200

    Started map editor and updated map mock

--- js/map_editor.js
+++ js/map_editor.js
@@ -2,26 +2,22 @@
 function populateEditSurface(information,container){
 	var editor = document.querySelector(container);
 	var editorTable = document.createElement('div');
-	setAttributes(editorTable,{'class':'data-editor-table','id':'chart-editor-table'});
+	setAttributes(editorTable,{'class':'data-editor-table','id':'map-editor-table'});
 	data = information.data;
 	for(var i = 0 ;i < data.length;i++){
-		var newSlice = document.createElement("div");
-		setAttributes(newSlice,{'class':'data-row','id':data[i].name,'data-row-count':i});
+		var newRow = document.createElement("div");
+		setAttributes(newRow,{'class':'data-row','id':data[i].name,'data-row-count':i});
 
-		var sliceLabel = document.createElement("label");
+		var regionLabel = document.createElement("label");
 		var text = document.createTextNode(data[i].name);  
-		sliceLabel.appendChild(text);
-
-		var colorInput = document.createElement("input");
-		setAttributes(colorInput,{"class":"color-input","type":"color","value":data[i].color});
+		regionLabel.appendChild(text);
 
 		var countInput = document.createElement("input");
 		setAttributes(countInput,{
 			"class":"count-input","type":"number","value":data[i].count});
-		newSlice.appendChild(colorInput);
-		newSlice.appendChild(sliceLabel);
-		newSlice.appendChild(countInput);
-		editorTable.appendChild(newSlice);	
+		newRow.appendChild(regionLabel);
+		newRow.appendChild(countInput);
+		editorTable.appendChild(newRow);	
 	}
 	editor.appendChild(editor);
 	var inputs =  document.querySelectorAll(container +' input');
